7697495141aa117dd6a8d2d82bad5bfcb674a054
bowmang19 <bowma446@umn.edu>
2021-06-03 15:46:30 -0500

    Finished 1662
    Finished 1662.Check If Two String Arrays are Equivalent

---
+++ 1662.Check If Two String Arrays are Equivalent/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        completeWord1 = '', completeWord2 = ''
+        for i in word1:
+            completeWord1 += i
+        for i in word2:
+            completeWord2 += i
+        if completeWord1 == completeWord2:
+            return True
+        return False
