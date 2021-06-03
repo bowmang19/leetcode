ff017a8da99ff11f7c28de20033fb5f67f06b7e4
bowmang19 <bowma446@umn.edu>
2021-06-03 15:47:26 -0500

    1662 Syntax Error
    Fixed Syntax error in 1662

--- 1662.Check If Two String Arrays are Equivalent/solution.py
+++ 1662.Check If Two String Arrays are Equivalent/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        completeWord1 = '', completeWord2 = ''
+        completeWord1 = ''
+        completeWord2 = ''
         for i in word1:
             completeWord1 += i
         for i in word2:
