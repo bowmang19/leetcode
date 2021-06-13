4c1fdb9a55f87001f6a0ab8443fcfd68d4afb0b9
bowmang19 <bowma446@umn.edu>
2021-06-12 22:41:53 -0400

    Updated 1470
    Updated 1470.Shuffle the Array to be more memory efficient (15th percentile to 91st percentile)

--- 1470.Shuffle the Array/solution.py
+++ 1470.Shuffle the Array/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         output = []
-        for i in range((len(nums)//2)):
+        for i in range(n):
             output.append(nums[i])
-            output.append(nums[i+((len(nums)//2))])
+            output.append(nums[i+n])
         return output
