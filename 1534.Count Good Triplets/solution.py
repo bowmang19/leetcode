015ffa68794e3c9f2303dea1feb4aceec48575ea
bowmang19 <bowma446@umn.edu>
2021-06-16 00:07:26 -0500

    Finished 1534
    Finished 1534.Count Good Triplets

---
+++ 1534.Count Good Triplets/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        counter = 0
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                if abs(arr[i] - arr[j]) > a:
+                    continue
+                for k in range(j+1, len(arr)):
+                    if abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        counter += 1
+        return counter
